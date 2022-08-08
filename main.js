3a00b9d5903e1222b33420086ae3e32fdfaa38dd
anshgirme <89573959+anshgirme@users.noreply.github.com>
2022-08-08 18:43:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+status = "";
+objects = [];
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detection Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
